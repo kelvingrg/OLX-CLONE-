94b6862f5bde5bea260d04cff5d92ff09d318eae
kelvin george <kelvinpktl@gmail.com>
2022-09-29 15:24:22 +0530

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+
+import firebase from "firebase";
+import 'firebase/firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDY658tyUWDMIK4rxhoPuxGdBupnKGgddo",
+    authDomain: "fir-e68a5.firebaseapp.com",
+    projectId: "fir-e68a5",
+    storageBucket: "fir-e68a5.appspot.com",
+    messagingSenderId: "640653402636",
+    appId: "1:640653402636:web:3b18efb6669effb275c429",
+    measurementId: "G-SH2ZRS9J53"
+  };
+
+  export default firebase.initializeApp(firebaseConfig)
